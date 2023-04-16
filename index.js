100971ad398133cbf0fcfe31a2e727a98c2fe98a
Eliakim Simões <eliakim.matos@sensedia.com>
2023-04-16 19:40:31 -0300

    dashboard update

--- index.js
+++ index.js
@@ -18,10 +18,11 @@ themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme-variables");
 
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
 
 //preenche pedidos na tabela
-Orders.forEach((order) => {
+Orders.forEach(order => {
   const tr = document.createElement("tr");
   const trContent = `
                         <td>${order.productName}</td>
