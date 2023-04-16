8d90c4e99fc9c1c9231aa85624bcf787c073eaf6
Eliakim Simões <eliakim.matos@sensedia.com>
2023-04-15 21:58:25 -0300

    dashboard

---
+++ index.js
@@ -0,0 +1,40 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+//mostrar sidebar
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+//fechar sidebar
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+//trocar tema
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+});
+
+//preenche pedidos na tabela
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${
+                          order.shipping === 'Cancelado'
+                            ? 'danger'
+                            : order.shipping === 'Pendente'
+                            ? 'warning'
+                            : 'primary'
+                        }">${order.shipping}</td>
+                        <td class="primary">Detalhes</td>`;
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
+});
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Mini Drone Dobravel",
+    productNumber: "85631",
+    paymentStatus: "Devido",
+    shipping: "Pendente",
+  },
+  {
+    productName: "larvender KF102 Drone",
+    productNumber: "36378",
+    paymentStatus: "Reembolsado",
+    shipping: "Cancelado",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    productNumber: "49347",
+    paymentStatus: "Devido",
+    shipping: "Pendente",
+  },
+  {
+    productName: "Drone com Drone Camera",
+    productNumber: "96996",
+    paymentStatus: "Pago",
+    shipping: "Entregue",
+  },
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "22821",
+    paymentStatus: "Pago",
+    shipping: "Entregue",
+  },
+  {
+    productName: "DJI Air 25",
+    productNumber: "81475",
+    paymentStatus: "Devido",
+    shipping: "Pendente",
+  },
+  {
+    productName: "Drone Lazenge",
+    productNumber: "00482",
+    paymentStatus: "Pago",
+    shipping: "Entregue",
+  }
+];
